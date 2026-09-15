737b7e29b0d5f4149cebeecb33ba1e2c33d02ad9
Bernardo <bernardo@email.com>
2026-09-15 11:38:47 -0300

    adicionada pastav15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,40 @@
+// let corinthias = "Hugo souza";
+// let cenoura = 99999999;
+// let numero = "9999999999999";
+// let idade = 15;
+// var nome = "bernardo";
+// nome = "pedro;"
+
+// const nome = " bernardo "
+// nome + "pedro";
+
+// idade = 20; 
+// idade = 21;
+
+// console.log("O hugo souza é o " + corinthias + " e ele tem " + idade + " anos");
+// crie 4 variaveis conteudo: nome, sobrenome, idade e curso
+// exiba no cosole em uma unica frase
+ 
+let nome = "bernardo";
+let sobrenome = " drokan";
+let idade = 15;
+let curso = "jovascript";
+
+// console.log( "meu nome é " + nome  +  sobrenome + " tenho " + idade + " anos " + "eu faço curso de " + curso);
+
+// let nome = "bernardo"; //string
+// let peso = 50; //int
+// let altura = 1.80; //float
+// let funcionario = false; //bool - true == verdadeiro ou false == falso
+
+// let cidade = null;
+// let estado;
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+console. log (" a variavel nome tem " + nome + "e é do tipo " + typeof nome)
+console. log (" a variavel sobrenome tem " + sobrenome + "e é do tipo " + typeof sobrenome)
+console. log (" a variavel idade tem " + idade + "e é do tipo " + typeof idade)
+console. log (" a variavel curso tem " + curso + "e é do tipo " + typeof curso)
